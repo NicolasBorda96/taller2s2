518fe1b594b8e3a9b74c7f05bd33493a0ba761a6
NicolasBorda96 <nicolas.borda@unillanos.edu.co>
2021-04-23 22:50:56 -0500

    Añade las acciones a los botones numericos

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -29,6 +29,11 @@ class _HomePageState extends State<HomePage> {
         Container(
           color: Colors.blue,
           height: 100,
+          child: Row(
+            children: [
+              Text(operaciones),
+            ],
+          ),
         ),
         Container(
           color: Colors.yellow,
@@ -37,35 +42,101 @@ class _HomePageState extends State<HomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("7")),
-                  ElevatedButton(onPressed: () {}, child: Text("8")),
-                  ElevatedButton(onPressed: () {}, child: Text("9")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "7";
+                        });
+                      },
+                      child: Text("7")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "8";
+                        });
+                      },
+                      child: Text("8")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "9";
+                        });
+                      },
+                      child: Text("9")),
                   ElevatedButton(onPressed: () {}, child: Text("/")),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("4")),
-                  ElevatedButton(onPressed: () {}, child: Text("5")),
-                  ElevatedButton(onPressed: () {}, child: Text("6")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "4";
+                        });
+                      },
+                      child: Text("4")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "5";
+                        });
+                      },
+                      child: Text("5")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "6";
+                        });
+                      },
+                      child: Text("6")),
                   ElevatedButton(onPressed: () {}, child: Text("*")),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("1")),
-                  ElevatedButton(onPressed: () {}, child: Text("2")),
-                  ElevatedButton(onPressed: () {}, child: Text("3")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "1";
+                        });
+                      },
+                      child: Text("1")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "2";
+                        });
+                      },
+                      child: Text("2")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "3";
+                        });
+                      },
+                      child: Text("3")),
                   ElevatedButton(onPressed: () {}, child: Text("-")),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("0")),
-                  ElevatedButton(onPressed: () {}, child: Text("C")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones += "0";
+                        });
+                      },
+                      child: Text("0")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          operaciones = "";
+                        });
+                      },
+                      child: Text("C")),
                   ElevatedButton(onPressed: () {}, child: Text("=")),
                   ElevatedButton(onPressed: () {}, child: Text("+")),
                 ],
